70a0753334d0d91cfe2e79f23474c60b1c9365e1
Allaparthi-vaishnavi <2311cs020027@mallareddyuniversity.ac.in>
2025-03-26 12:01:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 
 
 # app.py
+
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
@@ -18,21 +19,22 @@ model = joblib.load('iris_model.pkl')
 @app.route('/')
 def home():
     return render_template('home.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    #get data from form
-    try: 
+    # Get data from form
+    try:
         features = [float(request.form[f'feature{i}']) for i in range(1, 5)]
-    except ValueError: 
-        return render_template('result.html', prediction='Invalid input. Please enter numeric values.')
-    
+    except ValueError:
+        return render_template('result.html', prediction="Invalid input. Please enter numeric values.")
+
     # Make prediction
     prediction = model.predict([features])[0]
-    
-    # Map predicition to class name
+
+    # Map prediction to class name
     class_names = ['Setosa', 'Versicolor', 'Virginica']
     result = class_names[prediction]
-    
+
     return render_template('result.html', prediction=result)
 
 if __name__ == '__main__':
